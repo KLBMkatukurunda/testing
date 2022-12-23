278573974f1e7561c4df74f42b28ac341bdcdf48
Bimsara Manjitha <86804684+KLBMkatukurunda@users.noreply.github.com>
2022-12-23 13:59:26 +0530

    new edition

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,39 +1,44 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/header'
-import './index.css'
+import Header from '../components/header';
+import './index.css';
+import Footer from '../components/Footer';
 
 const Layout = ({ children, data }) => (
   <div>
-     <Header />
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-      { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
-   
-
-      {children()}
-    
+    <Header />
+    {children()}
+    <Footer data={data}>
+      Backgrounds made in Cinema 4D, iOS app in Swift, site in React.{' '}
+      <a href="mailto:support@designcode.io">Email us</a> to ask anything. ©
+      2018
+    </Footer>
   </div>
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.func,
-}
+};
 
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteTitleQuery {
     site {
       siteMetadata {
         title
+        description
+        keywords
       }
     }
   }
-`
+`;
